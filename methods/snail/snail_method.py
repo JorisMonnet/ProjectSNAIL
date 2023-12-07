e0982a8c3cd6d0d3a40eccdbcfbe31b67c2bea5a
ColinPelletier <colinpelletier@outlook.com>
2023-12-07 13:51:50 +0100

    Add code to convert fsb sequences to snail sequences

--- methods/snail/snail_method.py
+++ methods/snail/snail_method.py
@@ -24,6 +24,7 @@ class SnailMethod(MetaTemplate):
 
 
     def set_forward(self, x):
+        # TODO check that the labels are indeed correct
         y = torch.from_numpy(np.repeat(range(self.n_way), self.n_support + self.n_query))
         y = Variable(y.cuda())
 
@@ -55,6 +56,79 @@ class SnailMethod(MetaTemplate):
         return one_hot, idxs
     
     
+    def split_support_query_labels(self, original_tensor):
+        # Reshape the tensor to separate each 'way'
+        reshaped_tensor = original_tensor.view(self.n_way, self.n_support + self.n_query)
+
+        # Extract support and query tensors
+        support_tensor = reshaped_tensor[:, :self.n_support]
+        query_tensor = reshaped_tensor[:, self.n_support:]
+
+        # put both tensors in the fewshotbench format
+        # i.e. concat all supports together and all queries together
+        support_tensor = support_tensor.reshape(-1)
+        query_tensor = query_tensor.reshape(-1)
+
+        assert support_tensor.size()[0] == self.n_way * self.n_support
+        assert query_tensor.size()[0] == self.n_way * self.n_query
+
+        return support_tensor, query_tensor
+    
+    
+    def fsb_to_snail_seq_label(self, y_fsb):
+        """
+            Convert a sequence in the fewshotbench format to the snail format
+        """
+        # split support and queries
+        y_support, y_query = self.split_support_query_labels(y_fsb)
+
+        # sample n_query randomly from the query set and concatenate them to the support set
+        assert y_query.size == self.n_way * self.n_query
+        last_targets_labels = y_query[torch.randperm(y_query.size()[0])[:self.n_query]]
+
+        # put the labels and the last targets together
+        y_support = torch.cat((y_support, last_targets_labels), dim=0)
+        assert y_support.size()[0] == self.n_way * self.n_support + self.n_query
+
+        return y_support
+    
+    def split_support_query_data(self, original_tensor):
+        # Reshape the tensor to separate each 'way'
+        reshaped_tensor = original_tensor.view(self.n_way, self.n_support + self.n_query, *original_tensor.size()[1:])
+
+        # Extract support and query tensors
+        support_tensor = reshaped_tensor[:, :self.n_support]
+        query_tensor = reshaped_tensor[:, self.n_support:]
+
+        # put both tensors in the fewshotbench format
+        # i.e. concat all supports together and all queries together
+        support_tensor = support_tensor.reshape(-1, *original_tensor.size()[1:])
+        query_tensor = query_tensor.reshape(-1, *original_tensor.size()[1:])
+
+        assert support_tensor.size()[0] == self.n_way * self.n_support
+        assert query_tensor.size()[0] == self.n_way * self.n_query
+
+        return support_tensor, query_tensor
+    
+
+    def fsb_to_snail_seq_data(self, x_fsb):
+        """
+            Convert a sequence in the fewshotbench format to the snail format
+        """
+        # split support and queries
+        x_support, x_query = self.split_support_query_data(x_fsb)
+
+        # sample n_query randomly from the query set and concatenate them to the support set
+        assert x_query.size[0] == self.n_way * self.n_query
+        last_targets_data = x_query[torch.randperm(x_query.size()[0])[:self.n_query]]
+
+        # put the labels and the last targets together
+        x_support = torch.cat((x_support, last_targets_data), dim=0)
+        assert x_support.size()[0] == self.n_way * self.n_support + self.n_query
+
+        return x_support
+
+    
     def batch_for_few_shot(self, x, y):
         """
             Convert each labels in the batch to a one-hot vector
@@ -63,33 +137,37 @@ class SnailMethod(MetaTemplate):
                     y: (batch_size * seq_size, num_cls)
                     last_targets: (batch_size), the index of the label that we want to predict
         """
-        # seq_size = self.n_way * self.n_support + 1
-        seq_size = self.n_way * (self.n_support + self.n_query)
+        snail_seq_size = self.n_way * self.n_support + self.n_query
+        fsb_seq_size = self.n_way * (self.n_support + self.n_query)
+
         one_hots = []
         last_targets = []
-        print(f"Batch size: {4}, seq_size: {seq_size}", flush=True)
-        print(f"Y shape: {y.shape}", flush=True)
-        print(f"Y content: {y}", flush=True)
-        for i in range(4-1): # TODO use batch_size
-            print(f"Batch size: {4}, i: {i}, slice: [{i * seq_size}:{(i + 1) * seq_size}]", flush=True)
-            one_hot, idxs = self.labels_to_one_hot(y[i * seq_size : (i + 1) * seq_size])
-            print(f"one_hot shape: {one_hot.shape}", flush=True)
-            print(f"idxs shape: {len(idxs)}, content: {idxs}", flush=True)
+
+        assert y.size()[0] == fsb_seq_size * 4 # TODO use batch_size
+
+        # initialize the new x, which will have the snail_seq_size as first dimension
+        x_snail = torch.zeros((snail_seq_size * 4, *x.size()[1:])).cuda()
+        
+        for i in range(4): # TODO use batch_size
+            # convert the sequence from fewshotbench format to snail format
+            x_fsb = x[i * fsb_seq_size : (i + 1) * fsb_seq_size]
+            x_snail[i * snail_seq_size : (i + 1) * snail_seq_size] = self.fsb_to_snail_seq_data(x_fsb)
+
+            y_fsb = y[i * fsb_seq_size : (i + 1) * fsb_seq_size]
+            y_snail = self.fsb_to_snail_seq_label(y_fsb)
+
+            one_hot, idxs = self.labels_to_one_hot(y_snail)
             one_hots.append(one_hot)
-            last_targets.append(idxs[-1])
+            last_targets.append(idxs[-self.n_query:])
 
         # last_targets: the index of the label that we want to predict.
         # we only take the last one of each iter as we provide all the
         # previous ones as input to the model
-        last_targets = Variable(torch.Tensor(last_targets).long())
+        last_targets = Variable(torch.Tensor(last_targets).long()).cuda()
 
         # create a matrix of one-hot vectors
         one_hots = [torch.Tensor(temp) for temp in one_hots]
-        y = torch.cat(one_hots, dim=0)
-        x, y = Variable(x), Variable(y)
+        y_one_hot = torch.cat(one_hots, dim=0)
+        x_snail, y_one_hot = Variable(x_snail).cuda(), Variable(y_one_hot).cuda()
 
-        # put everything on cuda
-        x, y = x.cuda(), y.cuda()
-        last_targets = last_targets.cuda()
-
-        return x, y, last_targets
+        return x_snail, y_one_hot, last_targets
